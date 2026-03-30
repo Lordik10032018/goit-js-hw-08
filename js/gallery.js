c47ee374b16d18fb4dc3c97d3466c837778f5af2
Yaroslav_Savchenko <lordik10032018@gmail.com>
2026-03-30 12:16:58 +0300

    DZ-8-JS

--- js/gallery.js
+++ js/gallery.js
@@ -1,27 +1,88 @@
 const refs = {
     listItem: document.querySelector('.listItem'),
-    backdrop: document.querySelector('.backdrop'),
-    modal: document.querySelector('.modal'),
 }
 
-const images = [
-    { image: './images/1-1.jpg', id: 1 },
-    { image: './images/1-2.jpg', id: 2 },
-    { image: './images/1.jpg', id: 3 },
-    { image: './images/2-1.jpg', id: 4 },
-    { image: './images/2-2.jpg', id: 5 },
-    { image: './images/2.jpg', id: 6 },
-    { image: './images/3-2.jpg', id: 7 },
-    { image: './images/3.jpg', id: 8 },
-    { image: './images/4.jpg', id: 9 },
+    const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+   'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
 ];
 
+
+
 //#region make markup
-function markup({image, id}) {
-    return `<li class="item">
-          <button data-id="${id}" class="item-btn">
-            <img width="360" height="200" src="${image}" alt="image">
-          </button></li>`
+function markup({preview, original, description}) {
+    return `
+    <li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
 }
 
 function totalMarkup(array) {
@@ -31,44 +92,27 @@ const result = totalMarkup(images);
 refs.listItem.innerHTML = result;
 //#endregion
 
-//#region make modal
-//*=========================================
-
-function modalMarkup({image}) {
-    return `<img width="1100" height="600" src="${image}" alt="image">`       
-}
-
-function modalTotalMarkup(array) {
-    return array.map(modalMarkup).join('');
-}
-
-//*=========================================
-
 refs.listItem.addEventListener('click', (e) => {
-    if (!(e.target.closest('.item-btn'))) return;
-    refs.backdrop.classList.remove('is-hidden');
-    const btnElem = e.target.closest('.item-btn');
-    const filtered = images.find((img) => img.id == btnElem.dataset.id);
-    const result = modalMarkup(filtered);
-    refs.modal.innerHTML = result;
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') return;
+    const instance = basicLightbox.create(`
+        <div class="modal">
+        <a class="gallery-link" href="${e.target.dataset.source}">
+            <img src="${e.target.dataset.source}" alt="${e.target.alt}">
+            </a>
+        </div>
+    `)
+    
+    instance.show()
 })
 
-refs.backdrop.addEventListener('click', (e) => {
-    if (!(e.target === e.currentTarget)) return;
-    refs.backdrop.classList.add('is-hidden');
-    refs.modal.innerHTML = '';
-})
+//#region make modal
+
+
 //*=========================================
 
-window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') {
-        refs.backdrop.classList.add('is-hidden');
-        refs.modal.innerHTML = '';
-    }
-})
 
 //#endregion
-
 
 
 
